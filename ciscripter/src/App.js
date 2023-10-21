86f2f1e1c6b82e26af16bd0611bcb598f9526ace
Nikhil Saxena <nikhilsaxena9987@gmail.com>
2023-10-21 13:29:20 +0530

    initial commit installed cmirror

---
+++ ciscripter/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import codemirror from "./codemirror/codemirror";
+
+function App() {
+  return (
+    <div className="App">
+      <codemirror />
+    </div>
+  );
+}
+
+export default App;
